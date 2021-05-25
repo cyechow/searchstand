b4cc2e49c12e2ccad7da6bf4980df887dce97685
Edith Chow <edith.chow@arup.com>
2021-05-25 17:11:46 -0400

    Updated icons, put name to right side.

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import {
     AppBar,
+    Box,
+    Icon,
     Toolbar,
     Typography,
 } from '@material-ui/core';
 
+import Logo from './logo.svg'
+
 const AppHeader = () => (
     <AppBar position="static">
         <Toolbar>
+            <Box display='flex' flexGrow={1}>
+                <Icon fontSize='large'>
+                    <img src={Logo} height={32} width={32} />
+                </Icon>
+            </Box>
             <Typography variant="h6" color="inherit">
                 SearchStand
             </Typography>
